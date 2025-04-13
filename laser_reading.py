9da67718be3fcf843367046f8c406ee3a1137c2c
Mahsa Rabiei <mr6365@princeton.edu>
2025-04-13 16:02:07 -0400

    adding the laser reading code

---
+++ laser_reading.py
@@ -0,0 +1,61 @@
+import socket
+from datetime import date
+import time
+
+# Setup
+server_ip = '192.168.10.11'
+port = 8500
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((server_ip, port))
+today = date.today()
+now = time.localtime()
+
+
+path = 'C:/Users/mr6365/OneDrive - Princeton University/Desktop/laserProfiler_data_' + str(
+    today) + '_' + str(time.strftime("%H-%M-%S", now)) + '.txt'
+
+
+file = open(path, 'w')
+t_start = time.time()
+
+# Checking if we are receiving float values, not error or set up strings
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
+while True:
+
+    # Sending T1 message to receive data
+    msg = "T1"
+    client.send(msg.encode("utf-8")[:1024])
+
+    response = client.recv(1024)
+    response = response.decode("utf-8")
+    if response.lower() == "closed":
+        break
+    response = str(response)
+    response_str = response.split(',')
+
+    t_end = time.time() - t_start
+
+    if response_str[0] == 'ER' or 'T1' in response_str[1] or isfloat(response_str[1]) == False:
+        continue
+    width = float(response_str[0])
+    height = float(response_str[1])
+
+    file.write("width:{}, height: {}, t: {}".format(width, height, t_end))
+    file.write("width:{}, height:{}, time:{}".format(width, height, t_end))
+    file.write('\n')
+
+    print(width, height, t_end)
+
+
+client.close()
+print("Connection to server closed")
+
+
+# THIS CODE RUNS ONLY FOR OFFLINE SCANNING
